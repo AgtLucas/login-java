82fba711a746b8e09f28843194f6345af1584bf5
Lucas <metallucas.ds@gmail.com>
2013-08-19 22:34:11 -0300

    Update UserDAO

--- src/java/Authentication/UserDAO.java
+++ src/java/Authentication/UserDAO.java
@@ -1,6 +1,17 @@
 package Authentication;
 
+import java.util.ArrayList;
+
 public class UserDAO {
+    
+    public ArrayList<User> findAll() {
+        ArrayList<User> Users = new ArrayList<>();
+        
+        Users.add(this.getInstance("Lucas", "abc123"));
+        Users.add(this.getInstance("Admin", "123456"));
+        
+        return Users;
+    }
  
     public void getInstance() {
         
